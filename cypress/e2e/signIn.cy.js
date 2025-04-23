2dcb5f93d12317a5641f03a6963b8d9d55245636
олег иванов <you@example.com>
2025-04-23 14:46:08 +0200

    solve

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,45 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('https://the-internet.herokuapp.com/login');
   });
 
-  it('', () => {
+  it('should handle login scenarios and logout', () => {
+    // Login with valid credentials
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+    cy.get('button[type="submit"]').click();
 
+    // Assert successful login
+    cy.get('.flash.success').should(
+      'contain',
+      'You logged into a secure area!'
+    );
+    cy.url().should('include', '/secure');
+
+    // Logout
+    cy.get('a[href="/logout"]').click();
+
+    // Assert successful logout
+    cy.get('.flash.success').should(
+      'contain',
+      'You logged out of the secure area!'
+    );
+    cy.url().should('include', '/login');
+
+    // Login with invalid credentials
+    cy.get('#username').type('invalidUser');
+    cy.get('#password').type('invalidPass');
+    cy.get('button[type="submit"]').click();
+
+    // Assert validation error
+    cy.get('.flash.error').should('contain', 'Your username is invalid!');
   });
+});
+
+// Custom commands in cypress/support/commands.js
+Cypress.Commands.add('login', (username, password) => {
+  cy.get('#username').type(username);
+  cy.get('#password').type(password);
+  cy.get('button[type="submit"]').click();
 });
